84dc271effdd96b8276ce5f6db27480916913a81
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:05:50 -0300

    Created de Repository of resquest for printer installation

---
+++ InfrastructureServices/Repositories/Interfaces/IRequestForPrinterInstallation.cs
@@ -0,0 +1,14 @@
+﻿using InfrastructureServices.Model;
+
+namespace InfrastructureServices.Repositories.Interfaces
+{
+    public interface IRequestForPrinterInstallation
+    {
+        Task<List<RequestForPrinterInstallationModel>> FindAll();
+        Task<RequestForPrinterInstallationModel> FindById(int id);
+        Task<RequestForPrinterInstallationModel> Add(RequestForPrinterInstallationModel usuario);
+        Task<RequestForPrinterInstallationModel> Update(RequestForPrinterInstallationModel usuario, int id);
+        Task<bool> Delete(int id);
+
+    }
+}
---
+++ InfrastructureServices/Repositories/RequestForPrinterInstallationRepository.cs
@@ -0,0 +1,65 @@
+﻿using InfrastructureServices.Data;
+using InfrastructureServices.Model;
+using InfrastructureServices.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureServices.Repositories
+{
+    public class RequestForPrinterInstallationRepository : IRequestForPrinterInstallation
+    {
+        private readonly RequestSystemDBContext _dbContext;
+        public RequestForPrinterInstallationRepository(RequestSystemDBContext requestSystemDBContext) {
+            _dbContext = requestSystemDBContext;
+         }
+        public async Task<RequestForPrinterInstallationModel> FindById(int id)
+        {
+            return await _dbContext.Requests.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<RequestForPrinterInstallationModel>> FindAll()
+        {
+            return await _dbContext.Requests.ToListAsync();
+        }
+
+        public async Task<RequestForPrinterInstallationModel> Add(RequestForPrinterInstallationModel requestModel)
+        {
+            _dbContext.Requests.Add(requestModel);
+            await _dbContext.SaveChangesAsync();
+            return requestModel;
+        }
+
+        public async Task<RequestForPrinterInstallationModel> Update(RequestForPrinterInstallationModel requestModel, int id)
+        {
+            RequestForPrinterInstallationModel requestById = await FindById(id);
+
+            if(requestById == null)
+            {
+                throw new Exception($"Requisição para o ID: {id} não foi localizado no banco de dados.");
+            }
+
+            requestById.PrinterName = requestModel.PrinterName;
+            requestById.PrinterModel = requestModel.PrinterModel;
+            requestById.Location = requestModel.Location;
+            requestById.TypeOfConnection = requestModel.TypeOfConnection;
+            requestById.PersonResponsableForInstallation = requestModel.PersonResponsableForInstallation;
+
+            _dbContext.Requests.Update(requestById);
+            await _dbContext.SaveChangesAsync();
+            return requestById;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            RequestForPrinterInstallationModel requestById = await FindById(id);
+
+            if (requestById == null)
+            {
+                throw new Exception($"Requisição para o ID: {id} não foi localizado no banco de dados.");
+            }
+
+            _dbContext.Remove(requestById);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
