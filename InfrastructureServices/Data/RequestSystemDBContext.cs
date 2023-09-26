771c7406438fe827d2a46edc70bf415c680ee989
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:04:05 -0300

    Created the context db of resquest for printer installation

---
+++ InfrastructureServices/Data/RequestSystemDBContext.cs
@@ -0,0 +1,24 @@
+﻿using InfrastructureServices.Data.Map;
+using InfrastructureServices.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureServices.Data
+{
+    public class RequestSystemDBContext : DbContext
+    {
+        public RequestSystemDBContext(DbContextOptions<RequestSystemDBContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<RequestForPrinterInstallationModel> Requests { get; set; }
+        //public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new RequestForPrinterInstallationMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
