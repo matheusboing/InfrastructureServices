91d1e44778888557736a899c1d60c2ed0e5d8617
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:06:30 -0300

    Set up de database for the application

--- InfrastructureServices/Program.cs
+++ InfrastructureServices/Program.cs
@@ -1,3 +1,8 @@
+using InfrastructureServices.Data;
+using InfrastructureServices.Repositories;
+using InfrastructureServices.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<RequestSystemDBContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("DataBase")));
+builder.Services.AddScoped<IRequestForPrinterInstallation, RequestForPrinterInstallationRepository>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
