090bdaf8fd3839c6b7802afbf166de6c090c6e64
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:03:00 -0300

    Created Enums

---
+++ InfrastructureServices/Data/Enums/PaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace InfrastructureServices.Data.Enums
+{
+    public enum PaymentMethod
+    {
+        [Description("Semanalmente")]
+        Weekly = 0,
+        [Description("Mensalmente")]
+        Monthly = 1,
+        [Description("Anualmente")]
+        Anually = 2
+    }
+}
---
+++ InfrastructureServices/Data/Enums/TypeOfContract.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace InfrastructureServices.Data.Enums
+{
+    public enum TypeOfContract
+    {
+        [Description("CLT")]
+        Clt = 0,
+        [Description("PJ")]
+        Pj = 1
+    }
+}
