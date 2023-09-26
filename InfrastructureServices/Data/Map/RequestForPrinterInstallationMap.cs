a88353add85ccd12a21e788ec39c42339526d2c2
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:03:43 -0300

    Created the map of resquest for printer installation

---
+++ InfrastructureServices/Data/Map/RequestForPrinterInstallationMap.cs
@@ -0,0 +1,17 @@
+﻿using InfrastructureServices.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InfrastructureServices.Data.Map
+{
+    public class RequestForPrinterInstallationMap : IEntityTypeConfiguration<RequestForPrinterInstallationModel>
+    {
+        public void Configure(EntityTypeBuilder<RequestForPrinterInstallationModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.PrinterName).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.PrinterModel).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.PersonResponsableForInstallation).IsRequired().HasMaxLength(255);
+        }
+    }
+}
