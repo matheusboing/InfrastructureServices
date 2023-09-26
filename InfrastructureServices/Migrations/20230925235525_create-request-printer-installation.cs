baf544cf155ab8eabadaaf6417762ede4acd65ea
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:04:18 -0300

    Make migrations

---
+++ InfrastructureServices/Migrations/20230925235525_create-request-printer-installation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class createrequestprinterinstallation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    PrinterName = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false),
+                    PrinterModel = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false),
+                    Location = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
+                    TypeOfConnection = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
+                    PersonResponsableForInstallation = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
---
+++ InfrastructureServices/Migrations/20230926014510_add-installation-date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureServices.Migrations
+{
+    /// <inheritdoc />
+    public partial class addinstallationdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InstallationDate",
+                table: "Requests",
+                type: "TIMESTAMP(7)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstallationDate",
+                table: "Requests");
+        }
+    }
+}
