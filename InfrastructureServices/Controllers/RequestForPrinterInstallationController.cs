9f713b10cd7ef0f7a66dae1c3f07f1c7fd9ccdaa
Matheus Boing <matboing@hotmail.com>
2023-09-25 23:02:30 -0300

    Created the controller Request Printer Intallation

---
+++ InfrastructureServices/Controllers/RequestForPrinterInstallationController.cs
@@ -0,0 +1,56 @@
+﻿using InfrastructureServices.Model;
+using InfrastructureServices.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfrastructureServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestForPrinterInstallationController : ControllerBase
+    {
+        private readonly IRequestForPrinterInstallation _requestForPrinterInstallationRepository;
+
+        public RequestForPrinterInstallationController(IRequestForPrinterInstallation requestForPrinterInstallationRepository)
+        {
+            _requestForPrinterInstallationRepository = requestForPrinterInstallationRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<RequestForPrinterInstallationModel>>> FindAllPrinterRequests()
+        {
+            List<RequestForPrinterInstallationModel> requests = await _requestForPrinterInstallationRepository.FindAll();
+            return Ok(requests);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RequestForPrinterInstallationModel>> FindPrinterRequestById(int id)
+        {
+            RequestForPrinterInstallationModel request = await _requestForPrinterInstallationRepository.FindById(id);
+            return Ok(request);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RequestForPrinterInstallationModel>> AddPrinterRequests([FromBody] RequestForPrinterInstallationModel requestsModel)
+        {
+            RequestForPrinterInstallationModel request = await _requestForPrinterInstallationRepository.Add(requestsModel);
+            return Ok(request);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<RequestForPrinterInstallationModel>> UpdatePrinterRequest([FromBody] RequestForPrinterInstallationModel requestModel, int id)
+        {
+            requestModel.Id = id;
+            RequestForPrinterInstallationModel request = await _requestForPrinterInstallationRepository.Update(requestModel, id);
+            return Ok(request);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RequestForPrinterInstallationModel>> Delete(int id)
+        {
+            bool deleted = await _requestForPrinterInstallationRepository.Delete(id);
+            return Ok(deleted);
+        }
+
+    }
+}
